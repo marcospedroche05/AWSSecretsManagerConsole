6f501086001f7cd328f4148575cac940328005bf
xewi-11 <javi11coimbra@gmail.com>
2026-05-26 11:47:27 +0200

    Add project files.

---
+++ AWSSecretsManagerConsole/Models/KeysModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AWSSecretsManagerConsole.Models
+{
+    public class KeysModel
+    {
+        public string MySql { get; set; }
+        public string Bucket { get; set; }
+        public string Api { get; set; }
+    }
+}
---
+++ AWSSecretsManagerConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AWSSecretsManagerConsole.Helpers;
+using AWSSecretsManagerConsole.Models;
+using Newtonsoft.Json;
+
+Console.WriteLine("Ejemplo secretos AWS");
+string miSecret = await HelperSecretManager.GetSecretAsync();
+Console.WriteLine(miSecret);
+//MAPEAMOS EL STRING CON NUESTRO MODEL
+KeysModel model = JsonConvert.DeserializeObject<KeysModel>(miSecret);
+Console.WriteLine("MySql: " + model.MySql);
+Console.WriteLine("Bucket: " + model.Bucket);
+Console.WriteLine("Api: " + model.Api);
+Console.WriteLine("Todo correcto!!!!!!!");
+
