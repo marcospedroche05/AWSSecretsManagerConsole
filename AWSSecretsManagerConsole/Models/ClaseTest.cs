963c1b1378e2b66764f4eaf638b4d9f9436189b2
xewi-11 <javi11coimbra@gmail.com>
2026-05-26 12:44:22 +0200

    w

---
+++ AWSSecretsManagerConsole/Models/ClaseTest.cs
@@ -0,0 +1,17 @@
+﻿namespace AWSSecretsManagerConsole.Models
+{
+    public class ClaseTest
+    {
+        private KeysModel keys;
+
+        public ClaseTest(KeysModel keys)
+        {
+            this.keys = keys;
+        }
+
+        public string GetApiKey()
+        {
+            return this.keys.Api;
+        }
+    }
+}
--- AWSSecretsManagerConsole/Program.cs
+++ AWSSecretsManagerConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AWSSecretsManagerConsole.Helpers;
 using AWSSecretsManagerConsole.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 Console.WriteLine("Ejemplo secretos AWS");
@@ -12,4 +13,13 @@ Console.WriteLine("MySql: " + model.MySql);
 Console.WriteLine("Bucket: " + model.Bucket);
 Console.WriteLine("Api: " + model.Api);
 Console.WriteLine("Todo correcto!!!!!!!");
+
+var provider = new ServiceCollection()
+    .AddTransient<ClaseTest>()
+    .AddSingleton<KeysModel>(x => model)
+    .BuildServiceProvider();
+
+//LA INYECCION ESTA RESUELTA Y VAMOS A COMPROBAR LA FUNCIONALIDAD
+var test = provider.GetService<ClaseTest>();
+Console.WriteLine("Api Key: " + test.GetApiKey());
 
